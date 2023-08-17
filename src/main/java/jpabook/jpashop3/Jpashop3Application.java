9e6963d09240c3ca8868ea875544763db2daa682
refresh88 <ouok88@gmail.com>
2023-08-17 19:43:47 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop3/Jpashop3Application.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpashop3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpashop3Application.class, args);
+	}
+
+}
---
+++ src/test/java/jpabook/jpashop3/Jpashop3ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpashop3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
