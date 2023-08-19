e0ab001d514626accfd157df87da34f05dc10ce2
refresh88 <ouok88@gmail.com>
2023-08-19 13:20:36 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop3/domain/item/Item.java
+++ src/main/java/jpabook/jpashop3/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop3.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop3.domain.Category;
+import jpabook.jpashop3.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * 재고수량을 증가하는 로직
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity = quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
