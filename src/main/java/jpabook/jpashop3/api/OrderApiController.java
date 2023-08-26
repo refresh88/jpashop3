128b52aaff4a3f73f2692185730afe1926dfb817
refresh88 <ouok88@gmail.com>
2023-08-26 23:03:29 +0900

    JPA 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop3/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop3/api/OrderApiController.java
@@ -82,6 +82,11 @@ public class OrderApiController {
         return new Result(result);
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Data
     static class OrderDto {
 
